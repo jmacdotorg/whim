692adcd370e8c6ea779512a9c1f44418f06aa5f2
Jason McIntosh <jmac@jmac.org>
2020-05-31 19:16:36 -0400

    Making changes recommended in https://mojolicious.org/perldoc/Mojolicious/Guides/Cookbook#Making-your-application-installable

--- lib/Whim.pm
+++ lib/Whim.pm
@@ -1,6 +1,7 @@
 package Whim;
 use Mojo::Base 'Mojolicious';
 
+use Mojo::File qw(curfile);
 use Whim::Core;
 
 our $VERSION = '2020.05.18.00';
@@ -8,7 +9,18 @@ our $VERSION = '2020.05.18.00';
 sub startup {
     my $self = shift;
 
+    # Switch to installable home directory
+    $self->home(Mojo::Home->new(curfile->sibling('Whim')));
+
+    # Switch to installable "public" directory
+    $self->static->paths->[0] = $self->home->child('public');
+
+    # Switch to installable "templates" directory
+    $self->renderer->paths->[0] = $self->home->child('templates');
+
     push @{ $self->commands->namespaces }, 'Whim::Command';
+
+    # Create a 'whim' helper containing our Whim::Core object
     $self->helper(
         whim => sub {
             state $whim = Whim::Core->new(
@@ -20,6 +32,7 @@ sub startup {
         }
     );
 
+    # Set up routes for the listener
     my $r = $self->routes;
 
     $r->get('/')->to('listen#default');
