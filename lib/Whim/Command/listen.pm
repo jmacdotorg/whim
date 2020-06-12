8d9de4f1f726ecb75ad12d8f2e5b82c12ccc423f
Jason McIntosh <jmac@jmac.org>
2020-06-12 11:11:36 -0400

    Switch `listen` to use a stock Prefork instance (with a Daemon::Control wrapper), no longer shoving Hypnotoad where it doesn't want to go.

--- lib/Whim/Command/listen.pm
+++ lib/Whim/Command/listen.pm
@@ -1,46 +1,116 @@
 package Whim::Command::listen;
 use Mojo::Base 'Mojolicious::Command';
 
-use Mojo::Server::Hypnotoad;
-
-use FindBin;
-
 has description =>
     'Listen for incoming webmentions (and other HTTP requests)';
-has usage => 'XXX Fill me in later XXX';
+has usage => sub { shift->extract_usage };
 
-use Getopt::Long qw(GetOptionsFromArray);
+use Mojo::Util qw(getopt);
+
+use Daemon::Control;
+
+use Mojo::Server::Prefork;
 my %options;
 
 sub run {
     my ( $self, @args ) = @_;
 
-    GetOptionsFromArray( \@args, \%options, qw(foreground help stop test) );
+    my $command = $ARGV[-1];
+    my $method  = "do_$command";
 
-    # The WHIM_HYPNOTOAD environment variable tells the `whim`
-    # executable that it's being run in "Hypnotoad context", adjusting
-    # its default behavior.
-    #
-    # It also tells *this* command, that we shouldn't mess with
-    # Hypnotoad's own environment variables any further.
-
-    unless ( $ENV{WHIM_HYPNOTOAD} ) {
-        foreach (qw(foreground stop test)) {
-            $ENV{ 'HYPNOTOAD_' . uc($_) } = $options{$_};
-        }
-
-        # XXX Nothing for "help" right yet, alas!
+    unless ( Daemon::Control->can($method) ) {
+        die $self->usage . "\n";
     }
 
-    $ENV{WHIM_HYPNOTOAD} = 1;
+    my $listener = $self->create_listener(@args);
 
-    my $toad = Mojo::Server::Hypnotoad->new;
+    exit Daemon::Control->new(
+        name         => "Whim listener",
+        program      => \&launch_listener,
+        program_args => [$listener],
+        pid_file     => $listener->pid_file,
+        fork         => 1,
+    )->$method;
+}
 
-    # XXX Someday, when we have app configuration, we will pass it into
-    #     the $toad server object right around here.
+sub launch_listener {
+    my ( $daemon_control, $listener ) = @_;
+    $listener->daemonize;
+    $listener->cleanup(1)->run;
+}
 
-    $toad->run("$FindBin::Bin/whim");
+sub create_listener {
+    my ( $self, @args ) = @_;
 
+    # This code is adapted from the Mojolicious::Command::prefork source.
+    my $listener = Mojo::Server::Prefork->new( app => $self->app );
+    getopt \@args,
+        'a|accepts=i' => sub { $listener->accepts( $_[1] ) },
+        'b|backlog=i' => sub { $listener->backlog( $_[1] ) },
+        'c|clients=i' => sub { $listener->max_clients( $_[1] ) },
+        'G|graceful-timeout=i' =>
+        sub { $listener->graceful_timeout( $_[1] ) },
+        'I|heartbeat-interval=i' =>
+        sub { $listener->heartbeat_interval( $_[1] ) },
+        'H|heartbeat-timeout=i' =>
+        sub { $listener->heartbeat_timeout( $_[1] ) },
+        'i|inactivity-timeout=i' =>
+        sub { $listener->inactivity_timeout( $_[1] ) },
+        'k|keep-alive-timeout=i' =>
+        sub { $listener->keep_alive_timeout( $_[1] ) },
+        'l|location=s' => \my @listen,
+        'P|pid-file=s' => sub { $listener->pid_file( $_[1] ) },
+        'p|proxy'      => sub { $listener->reverse_proxy(1) },
+        'r|requests=i' => sub { $listener->max_requests( $_[1] ) },
+        's|spare=i'    => sub { $listener->spare( $_[1] ) },
+        'w|workers=i'  => sub { $listener->workers( $_[1] ) };
+
+    $listener->listen( \@listen ) if @listen;
+
+    return $listener;
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Whim::Command::listen - Listen command
+
+=head1 SYNOPSIS
+
+  Usage: whim listen [OPTIONS] [stop | start | restart | status]
+
+  Examples:
+    whim listen start
+    whim listen -l http://*:8080 start
+    whim listen -l 'https://*:443?cert=./server.crt&key=./server.key' start
+    whim listen stop
+
+  Options:
+    -l, --location <location>            One or more locations you want to
+                                         listen on, defaults to the value of
+                                         MOJO_LISTEN or "http://*:3000"
+    -P, --pid-file <path>                Path to process id file, defaults to
+                                         "prefork.pid" in a temporary directory
+    -p, --proxy                          Activate reverse proxy support,
+                                         defaults to the value of
+                                         MOJO_REVERSE_PROXY
+    -w, --workers <number>               Number of workers, defaults to 4
+
+  Logs to the "log" directory of Whim's home directory (usually $HOME/.whim).
+
+  Additionaly, this program accepts all options supported by the Mojolicious
+  prefork server. Try `mojo help prefork` to see the full list.
+
+=head1 DESCRIPTION
+
+Whim's daemon. It's basically just a L<Daemon::Control> instance wrapped
+around a stock L<Mojo::Server::Prefork>.
+
+=head1 SEE ALSO
+
+L<whim>
+
+=cut
